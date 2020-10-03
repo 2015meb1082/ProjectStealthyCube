cd7d06a5d0ba076be79f9720b1bb0e9fb3b33599
2015meb1082 <abhishek.sainiss441998@gmail.com>
2020-10-03 17:49:18 +0530

    Added Draw gizmos for the guards path

---
+++ Assets/Scripts/GuardMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Transform path;
+
+    private void OnDrawGizmos()
+    {
+        Vector3 startPosition = path.GetChild(0).position;
+        Vector3 previousPosition = startPosition;
+        foreach (Transform wayPoint in path)
+        {
+            Gizmos.DrawSphere(wayPoint.position, 0.3f);
+            Debug.DrawLine(previousPosition, wayPoint.position, Color.green);
+            previousPosition = wayPoint.position;
+        }
+        Debug.DrawLine(previousPosition, startPosition, Color.green);
+    }
+}
