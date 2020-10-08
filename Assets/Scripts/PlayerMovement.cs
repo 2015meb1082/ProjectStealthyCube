3e38f1274909f532ce7687ce9e37a4955abf34df
2015meb1082 <abhishek.sainiss98@gmail.com>
2020-10-08 20:36:54 +0530

    Added rigid body movement. Wall collisions are now enabled

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
@@ -15,24 +16,35 @@ public class PlayerMovement : MonoBehaviour
 
     private float smoothedAngle;
 
+    private Rigidbody rb;
+    private Vector3 currentVelocity;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
         Vector3 inputDir = new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical")).normalized;
         float inputMagnitude = inputDir.magnitude;
-        if (inputMagnitude!=0) {
+        if (inputMagnitude != 0) {
             smoothedInputMagnitude = Mathf.SmoothDamp(smoothedInputMagnitude, inputMagnitude, ref smoothMoveVelocity, smoothTime);
-            transform.Translate(transform.forward * moveSpeed * smoothedInputMagnitude * Time.deltaTime, Space.World);
-     
+            // transform.Translate(transform.forward * moveSpeed * smoothedInputMagnitude * Time.deltaTime, Space.World);
+
             float angle = Mathf.Atan2(inputDir.x, inputDir.z) * Mathf.Rad2Deg;
-            smoothedAngle = Mathf.LerpAngle(smoothedAngle,angle,turnSpeed*Time.deltaTime);
-            transform.eulerAngles = Vector3.up * smoothedAngle;
+            smoothedAngle = Mathf.LerpAngle(smoothedAngle, angle, turnSpeed * Time.deltaTime);
+            // transform.eulerAngles = Vector3.up * smoothedAngle;
+            currentVelocity = transform.forward * moveSpeed * inputMagnitude;
         }
-        
-        
+        else {
+            currentVelocity = Vector3.zero;
+        }
+  
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + currentVelocity * Time.deltaTime);
+        rb.MoveRotation(Quaternion.Euler(0,smoothedAngle,0));
     }
 }
